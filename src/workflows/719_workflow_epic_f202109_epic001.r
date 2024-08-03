f28dff88245fa080e41f4b20b3a25e9323b943aa
Florencia Santiago <fsantiago@austral.edu.ar>
2024-08-03 07:31:43 -0300

    epic001

--- src/workflows/719_workflow_epic_f202109_epic001.r
+++ src/workflows/719_workflow_epic_f202109_epic001.r
@@ -442,7 +442,7 @@ wf_septiembre <- function( pnombrewf )
   FEintra_manual_base()
   #DR_drifting_base(metodo="rank_cero_fijo")
   FEhist_base()
-  FErf_attributes_base()
+  #FErf_attributes_base()
   CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
 
   ts9 <- TS_strategy_base9()
